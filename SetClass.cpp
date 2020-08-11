114308fb84e9ede576fe5e52e5714a006c40399e
rszalanski <ryanpyan@comcast.net>
2020-08-10 19:47:52 -0700

    Removed remaining references to calculateTotalWeight()

--- SetClass.cpp
+++ SetClass.cpp
@@ -3,15 +3,11 @@
 SetClass::SetClass() {
     numReps = 0;
     weight = 0;
-
-    calculateTotalWeight();
 }
 
 SetClass::SetClass(int newNumReps, double newWeight) {
     numReps = newNumReps;
     weight = newWeight;
-
-    calculateTotalWeight();
 }
 
 int SetClass::getNumReps() {
